7590242645184f5e9671b53329b78762c878e749
Jasur <jasurbekshomaqsudov2004@gmail.com>
2024-05-20 21:57:56 +0500

    fix

--- src/server.js
+++ src/server.js
@@ -2,35 +2,35 @@ import express from 'express'
 import axios from 'axios'
 import dotenv from 'dotenv'
 import cors from 'cors'
-dotenv?.config()
+dotenv.config()
 
 const app = express()
-const PORT = Number(process?.env?.PORT) || 3000
+const PORT = 2208
 
-app?.use(cors())
-app?.use(express?.json())
+app.use(cors())
+app.use(express.json())
 
-app?.use('/', async (req, res) => {
-  const TARGET_URL = String(req?.query?.url) || ''
-  if (!TARGET_URL) return res?.status(400)?.send('Missing target URL')
+app.use('/', async (req, res) => {
+  const TARGET_URL = String(req.query.url) || ''
+  if (!TARGET_URL) return res.status(400).send('Missing target URL')
 
   try {
     const response = await axios({
-      method: req?.method,
+      method: req.method,
       url: TARGET_URL,
-      data: req?.body,
+      data: req.body,
       headers: {
         ...req.headers,
-        host: new URL(TARGET_URL)?.host || '',
+        host: new URL(TARGET_URL).host || '',
       },
     })
 
-    return res?.send(response?.data)
+    return res.send(response.data)
   } catch (error) {
-    return res?.status(500).send('Error fetching the requested URL')
+    return res.status(500).send('Error fetching the requested URL')
   }
 })
 
-app?.listen(PORT, () => {
-  console?.log(`CORS Proxy server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`CORS Proxy server running on port ${PORT}`)
 })
