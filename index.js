dc318b100d3ebfbcab9d1285edb21d6418aa6e3a
Jasur <jasurbekshomaqsudov2004@gmail.com>
2024-05-20 21:34:14 +0500

    fix

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import express, { Express, Request, Response } from 'express'
-import axios, { AxiosResponse } from 'axios'
+import express from 'express'
+import axios from 'axios'
 import dotenv from 'dotenv'
 import cors from 'cors'
 dotenv?.config()
 
-const app: Express = express()
-const PORT: number = Number(process?.env?.PORT) || 3000
+const app = express()
+const PORT = Number(process?.env?.PORT) || 3000
 
 app?.use(cors())
 app?.use(express?.json())
 
-app?.use('/', async (req: Request, res: Response) => {
-  const TARGET_URL: string = String(req?.query?.url) || ''
+app?.use('/', async (req, res) => {
+  const TARGET_URL = String(req?.query?.url) || ''
   if (!TARGET_URL) return res?.status(400)?.send('Missing target URL')
-  
+
   try {
-    const response: AxiosResponse<any, any> = await axios({
+    const response = await axios({
       method: req?.method,
       url: TARGET_URL,
       data: req?.body,
